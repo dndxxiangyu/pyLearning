dc738068a0be69f5314c161d81eb3c7c3838848d
wuxaingyu <wuxiangyu@corp.netease.com>
2017-05-11 14:28:01 +0800

    [ADD]slots限制了类的属性，属性包括了方法属性

--- test_9.py
+++ test_9.py
@@ -1,11 +1,20 @@
 # 实例属性和对象属性
 
 class Student:
+    '''
+    slots表示：只允许对该类添加name、age属性
+    其中该属性包括了方法
+    slots对子类是不起作用的，除非在子类中也定义slots，这样子类属性现在会加上父类属性限制
+    '''
+    # __slots__ = ('name', 'age')
     name = 'Student'
+
+    def __init__(self, name):
+        self.name = name
 
 
 # 当定义了一个了属性后，这个属性虽然归类所有，但类的所欲实例都可以访问这个类属性
-student = Student()
+student = Student("dd")
 print(student.name)
 
 # 由于实例属性优先级比类属性高，所以会屏蔽掉类的name属性
@@ -26,11 +35,16 @@ student.set_age = MethodType(set_age, student)
 student.set_age(10)
 print(student.age)
 
+
 # 给class绑定方法，这样所有实例都可以调用该方法
 def set_score(a, score):
     print(a)
+
+
 def test():
     print('test')
+
+
 Student.set_score = set_score
 Student.test = test
 student = Student()
