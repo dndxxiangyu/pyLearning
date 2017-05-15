9e8193a9911ae97d565ffeb450a9f2d73358cffe
wuxaingyu <wuxiangyu@corp.netease.com>
2017-05-15 16:36:15 +0800

    [ADD]数据库

---
+++ test_18.py
@@ -0,0 +1,18 @@
+# 数据库
+
+import sqlite3
+# conn = sqlite3.connect('test.db')
+# cursor = conn.cursor()
+# cursor.execute('create table user (id varchar(20) primary key, name varchar(20))')
+# cursor.rowcount
+# cursor.close()
+# conn.commit()
+# conn.close()
+
+
+conn = sqlite3.connect('test.db')
+cursor = conn.cursor()
+
+cursor.execute("select * from user where id = ?", ('1',))
+values = cursor.fetchall()
+print(values)
